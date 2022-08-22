e8ce4f11a5ed8fde36f7e80d41da5addbdb1468e
Gabriele Cardosi <gabriele.cardosi@gmail.com>
2022-08-22 13:58:49 +0200

    Add Efesto benchmark

--- drools-yaml-rules-benchmark/src/main/java/org/drools/yaml/benchmark/DroolsDurableBenchmark.java
+++ drools-yaml-rules-benchmark/src/main/java/org/drools/yaml/benchmark/DroolsDurableBenchmark.java
@@ -1,7 +1,20 @@
 package org.drools.yaml.benchmark;
 
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
 import org.drools.yaml.api.context.RulesExecutor;
 import org.drools.yaml.api.notations.DurableNotation;
+import org.drools.yaml.compilation.RulesCompilationContext;
+import org.drools.yaml.compilation.model.JsonResource;
+import org.drools.yaml.runtime.RulesRuntimeContext;
+import org.drools.yaml.runtime.model.EfestoInputMap;
+import org.drools.yaml.runtime.model.EfestoOutputMatches;
+import org.drools.yaml.runtime.utils.InputMaps;
+import org.json.JSONObject;
+import org.kie.efesto.compilationmanager.api.service.CompilationManager;
+import org.kie.efesto.compilationmanager.api.utils.SPIUtils;
+import org.kie.efesto.runtimemanager.api.service.RuntimeManager;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Fork;
@@ -14,8 +27,6 @@ import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.annotations.Warmup;
 
-import java.util.concurrent.TimeUnit;
-
 @State(Scope.Benchmark)
 @Warmup(iterations = 5)
 @Measurement(iterations = 10)
@@ -26,17 +37,76 @@ public class DroolsDurableBenchmark {
     @Param({"1000", "10000", "100000"})
     private int eventsNr;
 
+    @Param({"efesto", "notEfesto"})
+    private String invocation;
+
     private RulesExecutor rulesExecutor;
+
+    private RuntimeManager runtimeManager;
+
+    private RulesRuntimeContext rulesRuntimeContext;
+
+    private long executorId;
 
     @Setup
     public void setup() {
         String jsonRule = "{ \"rules\": {\"r_0\": {\"all\": [{\"m\": {\"$ex\": {\"event.i\": 1}}}]}}}";
-        rulesExecutor = RulesExecutor.createFromJson(DurableNotation.INSTANCE, jsonRule);
+        switch (invocation) {
+            case "efesto":
+                setupEfesto(jsonRule);
+                break;
+            default:
+                setupNotEfesto(jsonRule);
+        }
     }
 
     @Benchmark
     @BenchmarkMode(Mode.AverageTime)
     public int benchmark() {
+        switch (invocation) {
+            case "efesto":
+                return benchmarkEfesto();
+            default:
+                return benchmarkNotEfesto();
+        }
+    }
+
+    private void setupEfesto(String jsonRule) {
+        CompilationManager compilationManager =
+                SPIUtils.getCompilationManager(false).orElseThrow(() -> new RuntimeException("Failed to retrieve " +
+                                                                                                     "CompilationManager"));
+        RulesCompilationContext ctx = RulesCompilationContext.create();
+        JsonResource resource = new JsonResource(jsonRule, DurableNotation.INSTANCE);
+        compilationManager.processResource(ctx, resource);
+        executorId = ctx.ruleExecutorId();
+        runtimeManager =
+                org.kie.efesto.runtimemanager.api.utils.SPIUtils.getRuntimeManager(false).orElseThrow(() -> new RuntimeException("Failed to retrieve RuntimeManager"));
+        rulesRuntimeContext = RulesRuntimeContext.create();
+    }
+
+    private void setupNotEfesto(String jsonRule) {
+        rulesExecutor = RulesExecutor.createFromJson(DurableNotation.INSTANCE, jsonRule);
+    }
+
+    private int benchmarkEfesto() {
+        int count = 0;
+        for (int i = 0; i < eventsNr; i++) {
+            // Doing inside the loop to recreate condition as of `benchmarkNotEfesto`
+            String json = "{ \"event\": { \"i\": \"Done\" } }";
+            Map<String, Object> factMap = new JSONObject(json).toMap();
+            EfestoInputMap efestoInputMap = InputMaps.processEvents(executorId, factMap);
+            EfestoOutputMatches matches = (EfestoOutputMatches) runtimeManager
+                    .evaluateInput(rulesRuntimeContext, efestoInputMap)
+                    .iterator().next();
+            count += matches.getOutputData().size();
+        }
+        if (count != eventsNr) {
+            throw new IllegalStateException("Matched " + count + " rules, expected " + eventsNr);
+        }
+        return count;
+    }
+
+    private int benchmarkNotEfesto() {
         int count = 0;
         for (int i = 0; i < eventsNr; i++) {
             count += rulesExecutor.processEvents("{ \"event\": { \"i\": \"Done\" } }").size();
---
+++ drools-yaml-rules-efesto/drools-yaml-rules-compilation/src/main/java/org/drools/yaml/compilation/service/KieCompilerServiceJson.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2022 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.drools.yaml.compilation.service;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.drools.yaml.api.context.HasRulesExecutorContainer;
+import org.drools.yaml.api.context.RulesExecutor;
+import org.drools.yaml.api.domain.RulesSet;
+import org.drools.yaml.compilation.model.JsonResource;
+import org.drools.yaml.compilation.model.RuleSetResource;
+import org.kie.efesto.compilationmanager.api.exceptions.KieCompilerServiceException;
+import org.kie.efesto.compilationmanager.api.model.EfestoCompilationContext;
+import org.kie.efesto.compilationmanager.api.model.EfestoCompilationOutput;
+import org.kie.efesto.compilationmanager.api.model.EfestoResource;
+import org.kie.efesto.compilationmanager.api.service.KieCompilerService;
+
+public class KieCompilerServiceJson implements KieCompilerService {
+
+    @Override
+    public boolean canManageResource(EfestoResource toProcess) {
+        return toProcess instanceof JsonResource;
+    }
+
+    @Override
+    public List<EfestoCompilationOutput> processResource(EfestoResource toProcess, EfestoCompilationContext context) {
+        if (!canManageResource(toProcess)) {
+            throw new KieCompilerServiceException(String.format("%s can not process %s",
+                                                                this.getClass().getName(),
+                                                                toProcess.getClass().getName()));
+        }
+
+        JsonResource jsonResource = (JsonResource) toProcess;
+        RulesExecutor rulesExecutor = RulesExecutor.createFromJson(jsonResource.getNotation(), jsonResource.getContent());
+
+        ((HasRulesExecutorContainer) context).register(rulesExecutor);
+        return Collections.emptyList();
+    }
+}
