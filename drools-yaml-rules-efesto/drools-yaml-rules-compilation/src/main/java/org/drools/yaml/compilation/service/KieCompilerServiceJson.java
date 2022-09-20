c334185830f0c7b34f8f2d7d5fe6d78473218a00
Gabriele Cardosi <gabriele.cardosi@gmail.com>
2022-09-20 13:58:05 +0200

    wip

--- drools-yaml-rules-efesto/drools-yaml-rules-compilation/src/main/java/org/drools/yaml/compilation/service/KieCompilerServiceJson.java
+++ drools-yaml-rules-efesto/drools-yaml-rules-compilation/src/main/java/org/drools/yaml/compilation/service/KieCompilerServiceJson.java
@@ -15,12 +15,15 @@
  */
 package org.drools.yaml.compilation.service;
 
+import java.io.Serializable;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 import org.drools.yaml.api.context.HasRulesExecutorContainer;
 import org.drools.yaml.api.context.RulesExecutor;
 import org.drools.yaml.api.domain.RulesSet;
+import org.drools.yaml.api.notations.RuleNotation;
 import org.drools.yaml.compilation.model.JsonResource;
 import org.drools.yaml.compilation.model.RuleSetResource;
 import org.kie.efesto.compilationmanager.api.exceptions.KieCompilerServiceException;
@@ -30,6 +33,8 @@ import org.kie.efesto.compilationmanager.api.model.EfestoResource;
 import org.kie.efesto.compilationmanager.api.service.KieCompilerService;
 
 public class KieCompilerServiceJson implements KieCompilerService {
+
+
 
     @Override
     public boolean canManageResource(EfestoResource toProcess) {
@@ -44,10 +49,78 @@ public class KieCompilerServiceJson implements KieCompilerService {
                                                                 toProcess.getClass().getName()));
         }
 
+
         JsonResource jsonResource = (JsonResource) toProcess;
+        // TODO: issue: this works only for the on-the-fly compilation or with an hack, anyway in the same JVM;
+        // in this case it works only because
+        // 1: both CompilationContext and RuntimeContext share access to the same singleton RulesExecutorContainer
+        // 2: at compile time, the instance of RulesExecutor is "stored" in the above singleton by ((HasRulesExecutorContainer)EfestoCompilationContext), and at runtime it is
+        // retrieved from it by ((HasRulesExecutorContainer)EfestoRuntimeContext)
+        // Of course, this is a very specific situation and a very specific hack: it could not possibly work with isolated compilation and runtime execution
+        // We could consider the idea to write in the IndexFile" the information/parameters required to instantiate a given class at runtime:
+        // e.g.: the compilation create the following "executable" resource
+        // [
+        //  {
+        //    "step-type": "executable",
+        //    "fri": {
+        //      "basePath": "/(rule_name)",
+        //      "model": "drl",
+        //      "fri": "/drl/(rule_name)"
+        //    },
+        //    "fullClassNames": [
+        //      "org.drools.yaml.api.context.RulesExecutor"
+        //    ],
+        //    "parameters": [
+        //      "(RuleNotation)",
+        //      "(json rule representation)"
+        //    ]
+        //  }
+        //]
+        // At runtime, the runtime service
+        // 1) retrieve the name of the class whose method is to be invoked (org.drools.yaml.api.context.RulesExecutor)
+        // 2) retrieve the parameters ((RuleNotation) and (json rule representation))
+        // 3) invoke the method RulesExecutor.createFromJson(jsonResource.getNotation(), jsonResource.getContent());
+        // This works because the runtimeservice "knows" which method to invoke - In that specific case, it is also useless to store the
+        // "org.drools.yaml.api.context.RulesExecutor" name, since we already know it this to be used; but it may be useful in case of polymorphism.
+        // THis solution
+        // 1) would be more in line with the general design
+        // 2) it would be "almost" like what is already in place (e.g. PMML method loadFactory), with the only difference of the parameters
+        // 2) would always work (regardless of when compilation and runtime are executed)
+        // 3) would avoid the needs of instantiate an object at compile time to "bring it around" for runtime
+        // 4) would avoid the need of the "singleton" shared by both compilation and runtime
+        //
+
         RulesExecutor rulesExecutor = RulesExecutor.createFromJson(jsonResource.getNotation(), jsonResource.getContent());
 
         ((HasRulesExecutorContainer) context).register(rulesExecutor);
         return Collections.emptyList();
+    }
+
+    private class RulesExecutorKey implements Serializable {
+
+        private final RuleNotation ruleNotation;
+        private final int hashCode;
+
+        public RulesExecutorKey(RuleNotation ruleNotation, int hashCode) {
+            this.ruleNotation = ruleNotation;
+            this.hashCode = hashCode;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            RulesExecutorKey that = (RulesExecutorKey) o;
+            return hashCode == that.hashCode && Objects.equals(ruleNotation, that.ruleNotation);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(ruleNotation, hashCode);
+        }
     }
 }
